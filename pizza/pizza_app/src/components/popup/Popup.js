e55844d0c31e41e3c5b3f55fd76cb61fb8b3e2b0
AnShepetko <111201419+AnShepetko@users.noreply.github.com>
2022-08-14 18:10:42 +0300

    pizza

--- pizza/pizza_app/src/components/popup/Popup.js
+++ pizza/pizza_app/src/components/popup/Popup.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import './popup.style.css'
+import {useContext} from "react";
+import {MyContext} from "../../App";
+import Pizza from "../secondPage/Pizza";
 
 const Popup = ({onClick}) => {
+    let [orderCard, setOrderCard] = useContext(MyContext)
     return (
         <div className='popup'>
             <div className='popup__body'>
                 <div className='popup__content'>
                     <div className='popup__title'>Підтвердіть замовлення</div>
                     <div>
+                        {orderCard.map(el => {
+                            if (el.quantity > 0){
+                                return <Pizza title={el.title} image={el.image} price={el.price} quantity={el.quantity}/>
+                            }else{
+                                return  ''
+                            }
+                        })}
                     </div>
-                    <div onClick={onClick}>Close</div>
+                    <div className='popup__button_close' onClick={onClick}>Close</div>
                 </div>
             </div>
         </div>
